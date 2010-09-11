650db8f352e24521d454b696cbe8cd0000c0b697
rdubini <rdubini@1153d56f-347c-fb48-a2cf-f574bdf08831>
2010-09-11 15:51:06 +0000

    -ContainerInsertion

--- Datos/trunk/TpDatos/src.datos.BTreeSharp/ContainerInsertion.cpp
+++ Datos/trunk/TpDatos/src.datos.BTreeSharp/ContainerInsertion.cpp
@@ -15,3 +15,26 @@ ContainerInsertion::ContainerInsertion() {
 ContainerInsertion::~ContainerInsertion() {
 	// TODO Auto-generated destructor stub
 }
+const Registry* ContainerInsertion::getRegMidleKey() const throw() {
+	return this->midleRegKey;
+}
+
+void ContainerInsertion::setRegMidleKey(Registry* reg) throw() {
+	this->midleRegKey = reg;
+}
+
+unsigned int ContainerInsertion::getLeftBlock() const throw(){
+	return this->leftBlock;
+}
+
+void ContainerInsertion::setLeftBlock(unsigned int numBlock) throw() {
+	this->leftBlock = numBlock;
+}
+
+unsigned int ContainerInsertion::getRightBlock() const throw(){
+	return this->rigthBlock;
+}
+
+void ContainerInsertion::setRightBlock(unsigned int numBlock) throw(){
+	this->rigthBlock = numBlock;
+}
--- Datos/trunk/TpDatos/src.datos.BTreeSharp/ContainerInsertion.h
+++ Datos/trunk/TpDatos/src.datos.BTreeSharp/ContainerInsertion.h
@@ -8,10 +8,53 @@
 #ifndef CONTAINERINSERTION_H_
 #define CONTAINERINSERTION_H_
 
+#include "../src.datos.models/Registry.h"
 class ContainerInsertion {
 public:
 	ContainerInsertion();
 	virtual ~ContainerInsertion();
+
+	/**
+	 * Establece el regsitro con la clve de dato media luego de una division de bloques.
+	  */
+	void setRegMidleKey(Registry* reg) throw();
+	/**
+	 * Obtiene el registro con la clave de dato media luego de una division de bloques.
+	 */
+	const Registry* getRegMidleKey() const throw();
+	/**
+	 * Establece el numero de bloque izquierdo luego de una division de bloques.
+	  */
+	void setLeftBlock(unsigned int numBlock) throw();
+	/**
+	 * Obtiene el numero de bloque izquierdo luego de una division de bloques.
+	 */
+	unsigned int getLeftBlock() const throw();
+	/**
+	 * Establece el numero de bloque derecho luego de una division de bloques.
+	  */
+	void setRightBlock(unsigned int numBlock) throw();
+	/**
+	 * Obtiene el numero de bloque derecho luego de una division de bloques.
+	 */
+	unsigned int getRightBlock() const throw();
+private:
+	/**
+	 * Almacena el registro con la clave de dato media luego de una division de bloques.
+	 */
+	Registry* midleRegKey;
+	/**
+	 * Almacena el numero de bloque izquierdo luego de una division de bloques.
+	 */
+	unsigned int leftBlock;
+	/**
+	 * Almacena el numero de bloque derecho luego de una division de bloques.
+	 */
+	unsigned int rigthBlock;
+
+
+
 };
+
 
 #endif /* CONTAINERINSERTION_H_ */
