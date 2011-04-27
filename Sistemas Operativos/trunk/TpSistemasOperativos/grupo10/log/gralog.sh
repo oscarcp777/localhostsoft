545e2b9ee5d9a2f807d5c35566d8892e2b203810
rdubini <rdubini@1153d56f-347c-fb48-a2cf-f574bdf08831>
2011-04-27 19:20:52 +0000

    -Subo el log, no tiene muchas modificaciones, solo algunas y me falta cambiar mas cosas, pero se puede usar asi como esta, para correrlo los parametros son primero nombre de archivo y despues mensaje (tambien puede ser archivo, tipo mens y mens) pero lo del tipo de mens todavia no lo usen q lo tengo q cambiar

---
+++ Sistemas Operativos/trunk/TpSistemasOperativos/grupo10/log/gralog.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Comando "gralog"
+
+# Output
+# 	Archivo de log
+#
+# Opciones y Parámetros
+#	Parámetro 1 (obligatorio): comando
+#	Parámetro 2 (opcional): tipo de mensaje 
+#	Parámetro 3 (obligatorio): mensaje
+
+# No pueden ser más de 3 ni menos de 2 parámetros
+
+#TODO HAY Q VER LO DE LAS EXTENSIONES PQ SON POR DEFECTO .LOG PERO EL USUARIO PUEDE CAMBIARLO EN UNA VARIABLE LOGEXT
+
+if [ $# -gt 3 -o $# -lt 2 ]
+then
+	./gralog.sh gralog es "$1: Cantidad de parámetros inválida."
+	exit 1
+fi
+
+# Si es del instalador va en otro lado (TODO si el comando es instula el archivo de Log llamado instula.log que se graba en el directorio $grupo/conf) VER ESTO
+if [ -z $LOGDIR ]
+then
+	logdir="../log"
+else
+	logdir="$GRUPO/$LOGDIR"
+fi
+
+# Que la dirección no sea un archivo
+
+if [ -f $logdir ]
+then
+	./gralog.sh gralog es "$1: La dirección del directorio de destino es un archivo."
+	exit 1
+fi
+
+# Si no existe lo crea.
+if [ ! -d $logdir ]
+then
+	mkdir $logdir
+fi
+
+log=$logdir/$1.log
+
+# Si el archivo no existe lo crea
+touch $log  
+
+tipoMensaje="  "
+mensaje=""
+
+# Si hay dos parámetros no tengo tipo de mensaje
+
+if [ $# -eq 2 ]
+then
+	mensaje=$2
+fi
+
+# Si hay tres parámetros tengo tipo
+
+if [ $# -eq 3 ]
+then
+	tipoMensaje=$(echo $2 | tr "[:lower:]" "[:upper:]")
+	mensaje=$3
+fi
+
+# Si el tipo de mensaje es inválido lo pone en el log del gralog
+if [ $tipoMensaje != i -a $tipoMensaje != I -a $tipoMensaje != w -a $tipoMensaje != W -a $tipoMensaje != e -a $tipoMensaje != E -a $tipoMensaje != es -a $tipoMensaje != ES ]
+then
+	tipoMensaje="  "
+	./gralog.sh gralog w "$1: Tipo de mensaje inválido."
+else
+	if [ $tipoMensaje != es -a $tipoMensaje != ES ]
+	then
+		tipoMensaje=$tipoMensaje" "
+		
+	fi
+fi
+
+unaCadena=$tipoMensaje/
+
+tamanioArchivo=`stat -c%s $log`
+
+# Si el tamaño es menor a $LOGSIZE y no es instalacion puede haber que recortar
+if [ $LOGSIZE ]
+then
+	if [ $tamanioArchivo -eq $LOGSIZE ] || [ $tamanioArchivo -gt $LOGSIZE ]
+	then
+		# Lo paso a un archivo nuevo, borro el viejo y renombro
+		lineas=`wc -l $log | cut -d ' ' -f 1`
+		if [ $lineas -gt 50 ]
+		then
+			logtemp=$log.temp
+			touch $logtemp
+			tail -q -n 50 $log > $logtemp
+			rm $log
+			mv $logtemp $log
+		fi
+	fi
+fi
+# Escribo el archivo incluyendo nombre de usuario y fecha
+usuario=`whoami`
+fecha=`date +"%X %x"`
+echo "$usuario $fecha $1 $tipoMensaje $mensaje" >> $log
+
+exit 0 
