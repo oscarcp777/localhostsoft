931206bee23ddbc96f3e2609b138255380e64562
rdubini <rdubini@1153d56f-347c-fb48-a2cf-f574bdf08831>
2011-05-01 22:31:36 +0000

    -Preguntas al usuarios durante la intalacion, ACEPTA TERMINOS Y CONDICIONES??, valida perl version 5 e informa directorio al usuario

--- Sistemas Operativos/trunk/TpSistemasOperativos/grupo10/inst/instula.sh
+++ Sistemas Operativos/trunk/TpSistemasOperativos/grupo10/inst/instula.sh
@@ -20,8 +20,14 @@ function obtenerValor(){
 # Esta función recibe la lista de comandos instalados y los muestra junto con los no instalados
 function mostrarEstadoInstalacion(){
 	echo "INSTALADOS: " $1
-	#echo "NO INSTALADOS: "
-	echo "TODOS: " $2
+
+	#comparo las cadenas	
+	contador=0;
+	#while [ $contador -lt ${#COMANDOS} ]; do
+		#var1= echo $1 | cut -d ',' -f `expr $contador + 1`
+	#	echo ${COMANDOS[$contador]}
+	#contador=`expr $contador + 1`
+	#done
 }
 
 # Esta función recibe un comando y una ubicación y instala el mismo
@@ -60,32 +66,99 @@ fi
 if [ -e "$GRUPO/conf/instula.log" ]; then
 	# Hay instalación previa
 	# Renombra el archivo anterior y crea el nuevo
-	echo "TODO - renombrar archivo de log si es q existe y crear el nuevo"
+	echo "TODO - renombrar archivo de log si es q existe y crear el nuevo" 
 fi
 
 $GRALOG instula I "Inicio de Instalación"
 echo "Inicio de Instalación"
 
 # Valida si existe una instalación previa
-#if [ ! -e "$GRUPO/instula.conf" ]; then
-#	echo "El programa ya se encuentra instalado"
-#	$GRALOG instula I "El programa ya se encuentra instalado"
-#	echo "Verificando componentes ya instalados del programa..."
-#	$GRALOG instula I "Verificando componentes ya instalados del programa..."
-#		
-#	# Verifica cuales componentes estan instalados y cuales no
-#	comandosInstalados=`obtenerValor 23`
-#	`mostrarEstadoInstalacion $comandosInstalados $COMANDOS` 
-#	$GRALOG instula I "Borrar los componentes instalados y ejecutar el comando nuevamente"
-#	echo "Borrar los componentes instalados y ejecutar el comando nuevamente"
-#else
-#	echo "TODO" # TODO renombrar archivo de log si es q existe y crear el nuevo
-#fi
+if [ ! -e "$GRUPO/instula.conf" ]; then
+	echo "El programa ya se encuentra instalado"
+	$GRALOG instula.log I "El programa ya se encuentra instalado"
+	echo "Verificando componentes ya instalados del programa..."
+	$GRALOG instula.log I "Verificando componentes ya instalados del programa..."
+		
+	# Verifica cuales componentes estan instalados y cuales no
+	comandosInstalados=`obtenerValor 23`
+	mostrarEstadoInstalacion $comandosInstalados
+	$GRALOG instula.log I "Borrar los componentes instalados y ejecutar el comando nuevamente"
+	echo "Borrar los componentes instalados y ejecutar el comando nuevamente"
+else
+	echo "TODO" # TODO renombrar archivo de log si es q existe y crear el nuevo
+fi
+
 
 
 #---------ZONA RICHY---------------
+	# Consulta al usuario si esta de acuerdo con los terminos y condiciones de la instalacion
+	$GRALOG instula.log I "Mostrando mensaje de Aceptacion de terminos y condiciones...";
 
+	respVal="";
+	while [ -z $respVal ]	# mientras no responda si o no
+	do
+		clear;
+		
+echo '**************************************************************
+* Proceso de Instalación del sistema Postulantes             *
+*          Copyright TPSistemasOp (c)2011                    *
+**************************************************************
+* Al instalar POSTULA UD. expresa estar en un todo de acuerdo* 
+* con los términos y condiciones del "ACUERDO DE LICENCIA DE *
+* SOFTWARE" incluido en este paquete.                        *
+**************************************************************';
 
+		read -p "Acepta? SI - NO: " resp; #leo la respuesta y la guardo en $resp
+
+		respVal=$(echo $resp | grep '^[Ss][Ii]$\|^[Nn][Oo]$'); #valido la respuesta 
+
+	done
+
+	# transformo la respuesta a minuscula
+	respVal=$(echo $respVal | sed 's/^[Nn][Oo]$/no/');
+
+	# si el usuario no acepta finalizo el script
+	if [ $respVal = "no" ]; then
+		$GRALOG instula.log I  "Usuario NO acepto ACUERDO DE LICENCIA DE SOFTWARE";
+		exit 2;
+	fi
+	
+	# Usuario Acepto los terminos
+	$GRALOG instula.log I  "Usuario acepto ACUERDO DE LICENCIA DE SOFTWARE";
+
+	# Verifica la instalacion de perl
+	$GRALOG instula.log I "Verificando versión de Perl instalada....";
+	echo "Verificando versión de Perl instalada....";
+    PERLV=$(perl -v | grep 'v[0-9]\.[0-9]\+\.[0-9]*' -o); # obtengo la version de perl
+	numPERLV=$(echo $PERLV | cut -d"." -f1 | sed 's/^v\([0-9]\)$/\1/'); #obtengo el primer numero
+
+	#si perlv no existe o es menor a 5 mando error
+	if [ -z "$numPERLV" ] || [ $numPERLV -lt 5 ] ; then
+		msgPerl="**************************************************************
+				 * Para instalar POSTULA es necesario contar previamente con  *
+				 * Perl 5 o superior instalado.                               *
+				 * Efectúe su instalación e inténtelo nuevamente              *
+				 **************************************************************
+				 Proceso de Instalación Cancelado"
+		echo -e $msgPerl;
+		$GRALOG instula.log E $msgPerl;
+		exit 3;
+	else
+		echo "PERL instalado. Version:$PERLV";
+		$GRALOG instula.log I "PERL instalado. Version:$PERLV";
+	fi
+	
+
+echo "Todos los directorios del sistema de postulantes serán subdirectorios de:" 
+echo $GRUPO
+
+echo "Todos los componentes de la instalación se obtendrán del repositorio:" 
+echo $GRUPO"/inst"
+echo "Contenido del directorio:"
+ls $GRUPO/inst 
+
+echo "El archivo de configuración y el log de la instalación se registrarán en:" 
+echo $GRUPO"/conf"
 
 
 #---------FIN ZONA RICHY---------------
