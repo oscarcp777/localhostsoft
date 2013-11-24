1a551f4a0572de11bec0570e580c9d721334a548
caceres.oscar7@gmail.com <caceres.oscar7@gmail.com@1153d56f-347c-fb48-a2cf-f574bdf08831>
2013-11-24 22:42:16 +0000

    listo ya ta terminado el tp

---
+++ trunk/Cim2/src/ar/fi/uba/cim2/model/Tanques.java
@@ -0,0 +1,90 @@
+package ar.fi.uba.cim2.model;
+
+public class Tanques {
+	Double aguaGrisMax=20000.0;
+    public Double getAguaGrisMax() {
+		return aguaGrisMax;
+	}
+	public void setAguaGrisMax(Double aguaGrisMax) {
+		this.aguaGrisMax = aguaGrisMax;
+	}
+	public Double getFiltradoMax() {
+		return filtradoMax;
+	}
+	public void setFiltradoMax(Double filtradoMax) {
+		this.filtradoMax = filtradoMax;
+	}
+	public Double getTratamientoMax() {
+		return tratamientoMax;
+	}
+	public void setTratamientoMax(Double tratamientoMax) {
+		this.tratamientoMax = tratamientoMax;
+	}
+	public Double getClorificacionMax() {
+		return clorificacionMax;
+	}
+	public void setClorificacionMax(Double clorificacionMax) {
+		this.clorificacionMax = clorificacionMax;
+	}
+	public Double getTratadaMax() {
+		return tratadaMax;
+	}
+	public void setTratadaMax(Double tratadaMax) {
+		this.tratadaMax = tratadaMax;
+	}
+	public Double getCloroMax() {
+		return cloroMax;
+	}
+	public void setCloroMax(Double cloroMax) {
+		this.cloroMax = cloroMax;
+	}
+	public Double getAguaGris() {
+		return aguaGris;
+	}
+	public void setAguaGris(Double aguaGris) {
+		this.aguaGris = aguaGris;
+	}
+	public Double getFiltrado() {
+		return filtrado;
+	}
+	public void setFiltrado(Double filtrado) {
+		this.filtrado = filtrado;
+	}
+	public Double getTratamiento() {
+		return tratamiento;
+	}
+	public void setTratamiento(Double tratamiento) {
+		this.tratamiento = tratamiento;
+	}
+	public Double getClorificacion() {
+		return clorificacion;
+	}
+	public void setClorificacion(Double clorificacion) {
+		this.clorificacion = clorificacion;
+	}
+	public Double getTratada() {
+		return tratada;
+	}
+	public void setTratada(Double tratada) {
+		this.tratada = tratada;
+	}
+	public Double getCloro() {
+		return cloro;
+	}
+	public void setCloro(Double cloro) {
+		this.cloro = cloro;
+	}
+	Double filtradoMax=10000.0;
+    Double tratamientoMax=10000.0;
+    Double clorificacionMax=10000.0;
+    Double tratadaMax=20000.0;
+    Double cloroMax=0.0;
+	 Double aguaGris=0.0;
+	    Double filtrado=0.0;
+	    Double tratamiento=0.0;
+	    Double clorificacion=0.0;
+	    Double tratada=0.0;
+	    Double cloro=1000.0;
+		
+		
+}
